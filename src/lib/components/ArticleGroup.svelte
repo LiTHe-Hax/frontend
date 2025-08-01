aae2944d75daad48b91821131f45c53ab0069bb0
Viktor Holta <viktor.holta@outlook.com>
2025-08-01 22:06:07 +0200

    Refactor Section into Article & ArticleGroup

---
+++ src/lib/components/ArticleGroup.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	type Layout = "normal" | "thin";
+	type Props = { children: Snippet; layout?: Layout };
+	let { children, layout = "normal" }: Props = $props();
+</script>
+
+<div class={layout}>
+	{@render children()}
+</div>
+
+<style lang="scss">
+	@use "$lib/styles/size.scss";
+
+	$inner-mobile-gap: 1rem;
+	$outer-mobile-gap: 1rem;
+
+	$inner-desktop-gap: 1rem;
+	$outer-desktop-gap: 3rem;
+
+	div {
+		&.normal {
+			display: flex;
+			flex-direction: column;
+
+			@media screen and (max-width: size.$max-mobile-width) {
+				gap: $inner-mobile-gap;
+				margin: $outer-mobile-gap;
+			}
+
+			@media screen and (min-width: size.$min-desktop-width) {
+				gap: $inner-desktop-gap;
+				margin: $outer-desktop-gap;
+			}
+		}
+
+		&.thin {
+			display: flex;
+			flex-direction: column;
+
+			@media screen and (max-width: size.$max-mobile-width) {
+				gap: $inner-mobile-gap;
+				margin: $outer-mobile-gap;
+			}
+
+			@media screen and (min-width: size.$min-desktop-width) {
+				gap: $inner-desktop-gap;
+				margin: $outer-desktop-gap auto;
+				padding: 0 $outer-desktop-gap; // Padding imitates a horizontal margin
+				max-width: 39rem;
+			}
+		}
+	}
+</style>
