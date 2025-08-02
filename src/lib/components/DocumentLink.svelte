e26a94d95483b26e6bff0a325579b6a5d61f0a38
Viktor Holta <viktor.holta@outlook.com>
2025-08-02 12:06:12 +0200

    Refactor & redesign DocumentLink

--- src/lib/components/DocumentLink.svelte
+++ src/lib/components/DocumentLink.svelte
@@ -1,36 +1,51 @@
-<!-- <script lang="ts">
-    type Props = { link: string };
-    let { link }: Props = $props();
+<script lang="ts">
+	type Props = { href: string };
+	let { href }: Props = $props();
 
-    function deriveLabel() {
-        let splitLink = link.split("/");
-        return splitLink[splitLink.length - 1];
-    }
+	function deriveLabel() {
+		let splitHref = href.split("/");
+		return splitHref[splitHref.length - 1];
+	}
 
-    const label = $derived(deriveLabel());
-    const icon = "📄"; // TODO: switch to SVG?
+	function deriveIconClass() {
+		let splitHref = href.split(".");
+		let extension = splitHref[splitHref.length - 1];
+		switch (extension) {
+			case "pdf":
+				return "fa-file-pdf";
+			case "pptx":
+				return "fa-file-powerpoint";
+			case "7z":
+			case "zip":
+				return "fa-file-zipper";
+			default:
+				return "fa-file";
+		}
+	}
+
+	const label = $derived.by(deriveLabel);
+	const iconClass = $derived.by(deriveIconClass);
 </script>
 
-<span>
-    <a href={link}>
-        <span class="icon">{icon}</span>
-        <span class="label">{label}</span>
-    </a>
-</span>
+<a {href}>
+	<i class={["fa-solid", iconClass]}></i>
+	{label}
+</a>
 
-<style>
-    a {
-        display: inline-flex;
-        flex-direction: row;
-        gap: 0.2ch;
-        align-items: center;
-        color: var(--link-btn-fg);
-        text-decoration: none;
-    }
+<style lang="scss">
+	@use "$lib/styles/color.scss";
 
-    .label {
-        padding: 0.2em;
-        border-radius: 0.2rem;
-        background-color: var(--link-btn-bg);
-    }
-</style> -->
+	a {
+		display: inline-block;
+		padding: 0.2em;
+		border-radius: 0.2rem;
+		background: color.$gray-3;
+		color: color.$white;
+		word-break: break-all;
+		transition: background 150ms ease-out;
+
+		&:hover {
+			background: color.$gray-4;
+		}
+	}
+</style>
