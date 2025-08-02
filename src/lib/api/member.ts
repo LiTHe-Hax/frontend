b237f5ef9d2388fb198a66d0cbb692516a18c4a7
Viktor Holta <viktor.holta@outlook.com>
2025-08-02 18:52:28 +0200

    Refactor API modules

--- src/lib/api/member.ts
+++ src/lib/api/member.ts
@@ -1,13 +1,18 @@
-/* import request from "./request";
+import request from "./request";
 
-function createMember(firstName: string, lastName: string, email: string, isStudent: boolean) {
-    const data = {
-        "first_name": firstName,
-        "last_name": lastName,
-        "email": email,
-        "is_student": isStudent,
-    };
-    return request("members/", "post", data);
+function applyForMembership(
+	firstName: string,
+	lastName: string,
+	email: string,
+	isStudent: boolean,
+) {
+	const data = {
+		first_name: firstName,
+		last_name: lastName,
+		email: email,
+		is_student: isStudent,
+	};
+	return request("members/", "post", data);
 }
 
-export { createMember }; */
+export { applyForMembership };
