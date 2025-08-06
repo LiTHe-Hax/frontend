5b5d51989b0aa1f32252b5a219b24861e3c0addf
Viktor Holta <viktor.holta@outlook.com>
2025-08-06 20:14:17 +0200

    Run formatter & fix linting errors

--- src/lib/components/DocumentLink.svelte
+++ src/lib/components/DocumentLink.svelte
@@ -1,36 +1,36 @@
 <script lang="ts">
-    type Props = { link: string };
-    let { link }: Props = $props();
+	type Props = { link: string };
+	let { link }: Props = $props();
 
-    function deriveLabel() {
-        let splitLink = link.split("/");
-        return splitLink[splitLink.length - 1];
-    }
+	function deriveLabel() {
+		let splitLink = link.split("/");
+		return splitLink[splitLink.length - 1];
+	}
 
-    const label = $derived(deriveLabel());
-    const icon = "📄"; // TODO: switch to SVG?
+	const label = $derived(deriveLabel());
+	const icon = "📄"; // TODO: switch to SVG?
 </script>
 
 <span>
-    <a href={link}>
-        <span class="icon">{icon}</span>
-        <span class="label">{label}</span>
-    </a>
+	<a href={link}>
+		<span class="icon">{icon}</span>
+		<span class="label">{label}</span>
+	</a>
 </span>
 
 <style>
-    a {
-        display: inline-flex;
-        flex-direction: row;
-        gap: 0.2ch;
-        align-items: center;
-        color: var(--link-btn-fg);
-        text-decoration: none;
-    }
+	a {
+		display: inline-flex;
+		flex-direction: row;
+		gap: 0.2ch;
+		align-items: center;
+		color: var(--link-btn-fg);
+		text-decoration: none;
+	}
 
-    .label {
-        padding: 0.2em;
-        border-radius: 0.2rem;
-        background-color: var(--link-btn-bg);
-    }
+	.label {
+		padding: 0.2em;
+		border-radius: 0.2rem;
+		background-color: var(--link-btn-bg);
+	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-    import "$lib/styles/global.css"; // This stylesheet affects the entire site.
-    import LayoutHeader from "$lib/components/LayoutHeader.svelte";
+	import type { Snippet } from "svelte";
+	import "$lib/styles/global.css"; // This stylesheet affects the entire site.
+	import LayoutHeader from "$lib/components/LayoutHeader.svelte";
 
-    interface LayoutProps {
-        children: any;
-        data: {
-            canonicalHref: string;
-        };
-    }
+	interface LayoutProps {
+		children: Snippet;
+		data: {
+			canonicalHref: string;
+		};
+	}
 
-    // In Runes mode, we extract our data using $props()
-    let { children, data } = $props() as LayoutProps;
+	// In Runes mode, we extract our data using $props()
+	let { children, data } = $props() as LayoutProps;
 </script>
 
 <svelte:head>
-    <link rel="canonical" href="{data.canonicalHref}" />
-    <meta property="og:url" content="{data.canonicalHref}">
+	<link rel="canonical" href={data.canonicalHref} />
+	<meta property="og:url" content={data.canonicalHref} />
 </svelte:head>
 
 <LayoutHeader />
@@ -23,7 +24,7 @@
 <main>{@render children()}</main>
 
 <style>
-    main {
-        width: 100vw;
-    }
+	main {
+		width: 100vw;
+	}
 </style>
