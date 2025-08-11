6c3ab848814f4e8f5335027afe8b3c287ce62bb4
Viktor Holta <viktor.holta@outlook.com>
2025-08-11 15:59:51 +0200

    Add option to make Article translucent

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import type { Snippet } from "svelte";
 
-	const { children }: { children: Snippet } = $props();
+	const { children, translucent }: { children: Snippet; translucent?: boolean } = $props();
 </script>
 
-<article>
-	{@render children()}
+<article class={[translucent && "translucent"]}>
+	<div class="contents">
+		{@render children()}
+	</div>
 </article>
 
 <style lang="scss">
@@ -17,16 +19,38 @@
 		background-color: color.$black-2;
 		color: color.$white;
 
-		:global {
-			@include mixin.remove-top-bottom-child-margins();
-		}
-
 		@include mixin.on-mobile() {
 			padding: 1rem;
 		}
 
 		@include mixin.on-desktop() {
 			padding: 1.5rem;
+		}
+
+		.contents :global {
+			@include mixin.remove-top-bottom-child-margins();
+		}
+
+		&.translucent {
+			.contents {
+				@include mixin.unified-transition(150ms, ease, opacity);
+
+				opacity: 0.5;
+
+				:global(img) {
+					@include mixin.unified-transition(150ms, ease, filter);
+
+					filter: saturate(50%);
+				}
+			}
+
+			&:hover .contents {
+				opacity: 1;
+
+				:global(img) {
+					filter: saturate(100%);
+				}
+			}
 		}
 	}
 </style>
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -31,184 +31,174 @@
 <h2 class="section-header">Previous Events</h2>
 
 <ArticleGroup layout="thin">
-	<Article>
-		<div class="past-event">
-			<h1>Annual Meeting</h1>
-			<enhanced:img src={acas} alt="ACAS at Linköping University" />
-			<p>
-				We will be holding our annual meeting soon, which you're very welcome to attend! During the
-				meeting we'll talk about the last year of LiTHe Hax and we'll be voting on the next year of
-				LiTHe Hax. One thing that'll be voted on is who will be included in the next board, so this
-				is your chance to join and affect the future of LiTHe Hax.
-			</p>
-			<p>Coffee and fika will be provided to everyone who attend the meeting.</p>
-			<p><strong>Date & Time:</strong> 28 May 2025, 17:15</p>
-			<p>
-				<strong>Location:</strong>
-				<a
-					href="https://use.mazemap.com/?utm_medium=longurl#v=1&config=liu&campusid=742&zlevel=2&center=15.576810,58.402255&zoom=18&sharepoitype=poi&sharepoi=1000927810"
-				>
-					ACAS (Linköping University, Sweden)
-				</a>
-			</p>
-			<p>
-				<strong>Agenda:</strong>
-				<DocumentLink
-					link={asset("/documents/2024-2025/events/annual_meeting/kallelse_arsmote.pdf")}
-				/>
-			</p>
+	<Article translucent>
+		<h1>Annual Meeting</h1>
+		<enhanced:img src={acas} alt="ACAS at Linköping University" />
+		<p>
+			We will be holding our annual meeting soon, which you're very welcome to attend! During the
+			meeting we'll talk about the last year of LiTHe Hax and we'll be voting on the next year of
+			LiTHe Hax. One thing that'll be voted on is who will be included in the next board, so this is
+			your chance to join and affect the future of LiTHe Hax.
+		</p>
+		<p>Coffee and fika will be provided to everyone who attend the meeting.</p>
+		<p><strong>Date & Time:</strong> 28 May 2025, 17:15</p>
+		<p>
+			<strong>Location:</strong>
+			<a
+				href="https://use.mazemap.com/?utm_medium=longurl#v=1&config=liu&campusid=742&zlevel=2&center=15.576810,58.402255&zoom=18&sharepoitype=poi&sharepoi=1000927810"
+			>
+				ACAS (Linköping University, Sweden)
+			</a>
+		</p>
+		<p>
+			<strong>Agenda:</strong>
+			<DocumentLink
+				link={asset("/documents/2024-2025/events/annual_meeting/kallelse_arsmote.pdf")}
+			/>
+		</p>
 
-			<h3>Candidacy</h3>
-			<p>
-				Anyone can can run for the positions brought up during the meeting. You can oppose any of
-				the current nominees, which means that you can nominate yourself or someone else for that
-				position (without having been previously nominated). You can also submit an opposition by
-				sending a mail to <a href="mailto:ordforande@lithehax.se">ordforande@lithehax.se</a>, where
-				you explain which position you're interested in and why you are a suitable candidate.
-			</p>
-			<p>
-				If you are running for a position in the board you will have to present yourself during the
-				meeting and answer any potential questions. Below are some suggestions of what you may want
-				to bring up when presenting yourself:
-			</p>
-			<ul>
-				<li>Who you are</li>
-				<li>Why you are running for the position</li>
-				<li>Past experiences that may be of relevance</li>
-			</ul>
-			<p>
-				<strong>Current nominees:</strong>
-				<DocumentLink link={asset("/documents/2024-2025/events/annual_meeting/nominations.pdf")} />
-			</p>
+		<h3>Candidacy</h3>
+		<p>
+			Anyone can can run for the positions brought up during the meeting. You can oppose any of the
+			current nominees, which means that you can nominate yourself or someone else for that position
+			(without having been previously nominated). You can also submit an opposition by sending a
+			mail to <a href="mailto:ordforande@lithehax.se">ordforande@lithehax.se</a>, where you explain
+			which position you're interested in and why you are a suitable candidate.
+		</p>
+		<p>
+			If you are running for a position in the board you will have to present yourself during the
+			meeting and answer any potential questions. Below are some suggestions of what you may want to
+			bring up when presenting yourself:
+		</p>
+		<ul>
+			<li>Who you are</li>
+			<li>Why you are running for the position</li>
+			<li>Past experiences that may be of relevance</li>
+		</ul>
+		<p>
+			<strong>Current nominees:</strong>
+			<DocumentLink link={asset("/documents/2024-2025/events/annual_meeting/nominations.pdf")} />
+		</p>
 
-			<h3>Motions</h3>
+		<h3>Motions</h3>
 
-			<p>
-				Anyone can send in motions. The easiest way to do this is by mailing the motion to
-				<a href="mailto:ordforande@lithehax.se">ordforande@lithehax.se</a>. Motions must be sent in
-				writing to the board no later than seven days before the meeting (21 May 2025).
-			</p>
-			<p>
-				<strong>Current motions and propositions:</strong>
-				<DocumentLink
-					link={asset("/documents/2024-2025/events/annual_meeting/motions_and_propositions.pdf")}
-				/>
-			</p>
+		<p>
+			Anyone can send in motions. The easiest way to do this is by mailing the motion to
+			<a href="mailto:ordforande@lithehax.se">ordforande@lithehax.se</a>. Motions must be sent in
+			writing to the board no later than seven days before the meeting (21 May 2025).
+		</p>
+		<p>
+			<strong>Current motions and propositions:</strong>
+			<DocumentLink
+				link={asset("/documents/2024-2025/events/annual_meeting/motions_and_propositions.pdf")}
+			/>
+		</p>
+	</Article>
+
+	<Article translucent>
+		<h1>Foo Bar at Foo Café</h1>
+
+		<enhanced:img src={foobar} alt="Foo Bar at Foo Café" />
+		<p>
+			Foo Café and LiTHe Hax recently teamed up for a cybersecurity evening focused on Capture the
+			Flag challenges. The event featured a brief introduction, a problem-solving lecture, and a
+			chance to network over food and drinks with others passionate about cybersecurity.
+		</p>
+		<p><strong>Date & Time:</strong> 24 Apr 2025, 17:00-20:00</p>
+		<p>
+			<strong>Location:</strong>
+			<a href="https://maps.app.goo.gl/LNrr7CWQWwaH3deJ9">
+				Goto 10 Linköping (Teknikringen 7, 583 30 Linköping)
+			</a>
+		</p>
+		<p>
+			<strong>Presentation:</strong>
+			<DocumentLink
+				link={asset("/documents/2024-2025/events/foo_cafe/goto_10_presentation.pptx")}
+			/>
+		</p>
+
+		<hr />
+		<div class="sponsor-logos">
+			<enhanced:img src={goto10Logo} alt="Goto 10 logo" />
+			<img src={fooCafeLogo} alt="Foo Café logo" />
 		</div>
 	</Article>
 
-	<Article>
-		<div class="past-event">
-			<h1>Foo Bar at Foo Café</h1>
+	<Article translucent>
+		<h1>LiU CTF</h1>
 
-			<enhanced:img src={foobar} alt="Foo Bar at Foo Café" />
-			<p>
-				Foo Café and LiTHe Hax recently teamed up for a cybersecurity evening focused on Capture the
-				Flag challenges. The event featured a brief introduction, a problem-solving lecture, and a
-				chance to network over food and drinks with others passionate about cybersecurity.
-			</p>
-			<p><strong>Date & Time:</strong> 24 Apr 2025, 17:00-20:00</p>
-			<p>
-				<strong>Location:</strong>
-				<a href="https://maps.app.goo.gl/LNrr7CWQWwaH3deJ9">
-					Goto 10 Linköping (Teknikringen 7, 583 30 Linköping)
-				</a>
-			</p>
-			<p>
-				<strong>Presentation:</strong>
-				<DocumentLink
-					link={asset("/documents/2024-2025/events/foo_cafe/goto_10_presentation.pptx")}
-				/>
-			</p>
+		<enhanced:img src={cyberRoom} alt="Cybersecurity Room" />
+		<p>
+			The LiTHe Hax LiU CTF was an exciting Capture the Flag competition held on March 8. We hosted
+			this event to promote cybersecurity awareness within the student community, combining both
+			relevance and fun.
+		</p>
+		<p>
+			A total of 18 teams competed, each consisting of 2–5 participants. Free food was provided to
+			all participants.
+		</p>
+		<p><strong>Date & Time:</strong> 8 Mar 2025, 17:15-23:00</p>
+		<p>
+			<strong>Location:</strong>
+			<a
+				href="https://use.mazemap.com/?utm_medium=longurl#v=1&config=liu&campusid=742&zlevel=2&center=15.576432,58.398370&zoom=18&sharepoitype=poi&sharepoi=1002771474"
+			>
+				Brandväggen, Bakdörren and ISYtan (Linköping University, Sweden)
+			</a>
+		</p>
+		<p>
+			<strong>CTF Platform:</strong>
+			<a href="https://ctf.lithehax.se"> ctf.lithehax.se </a>
+		</p>
+		<p>
+			<strong>CTF rules:</strong>
+			<DocumentLink link={asset("/documents/2024-2025/events/liu_ctf/liu_ctf_rules.pdf")} />
+		</p>
 
-			<hr />
-			<div class="sponsor-logos">
-				<enhanced:img src={goto10Logo} alt="Goto 10 logo" />
-				<img src={fooCafeLogo} alt="Foo Café logo" />
-			</div>
+		<hr />
+		<div class="sponsor-logos">
+			<enhanced:img src={sickLogo} alt="sick logo" />
+			<enhanced:img src={sectraLogo} alt="sectra logo" />
+			<enhanced:img src={sylogLogo} alt="sylog logo" />
 		</div>
 	</Article>
 
-	<Article>
-		<div class="past-event">
-			<h1>LiU CTF</h1>
+	<Article translucent>
+		<h1>Workshop</h1>
+		<enhanced:img src={studenthuset} alt="Studenthuset" />
+		<p>
+			We held a workshop in preparation for our CTF. During the workshop, we explained and showcased
+			example challenges that resembled the actual CTF challenges.
+		</p>
+		<p>
+			There were 80 available seats, and those who registered were provided free food, sponsored by
+			the companies below.
+		</p>
+		<p><strong>Date & Time:</strong> 27 Feb 2025, 17:15-20:00</p>
+		<p>
+			<strong>Location:</strong>
+			<a
+				href="https://use.mazemap.com/#v=1&config=liu&campusid=742&zlevel=6&center=15.577904,58.397057&zoom=18.9&sharepoitype=poi&sharepoi=1000927265"
+			>
+				SH62 and SH63 (Linköping University, Sweden)
+			</a>
+		</p>
+		<p>
+			<strong>Workshop tasks:</strong>
+			<DocumentLink link={asset("/documents/2024-2025/events/workshop/workshop_challenges.zip")} />
+		</p>
+		<p>
+			<strong>Workshop presentation:</strong>
+			<DocumentLink
+				link={asset("/documents/2024-2025/events/workshop/workshop_presentation.pptx")}
+			/>
+		</p>
 
-			<enhanced:img src={cyberRoom} alt="Cybersecurity Room" />
-			<p>
-				The LiTHe Hax LiU CTF was an exciting Capture the Flag competition held on March 8. We
-				hosted this event to promote cybersecurity awareness within the student community, combining
-				both relevance and fun.
-			</p>
-			<p>
-				A total of 18 teams competed, each consisting of 2–5 participants. Free food was provided to
-				all participants.
-			</p>
-			<p><strong>Date & Time:</strong> 8 Mar 2025, 17:15-23:00</p>
-			<p>
-				<strong>Location:</strong>
-				<a
-					href="https://use.mazemap.com/?utm_medium=longurl#v=1&config=liu&campusid=742&zlevel=2&center=15.576432,58.398370&zoom=18&sharepoitype=poi&sharepoi=1002771474"
-				>
-					Brandväggen, Bakdörren and ISYtan (Linköping University, Sweden)
-				</a>
-			</p>
-			<p>
-				<strong>CTF Platform:</strong>
-				<a href="https://ctf.lithehax.se"> ctf.lithehax.se </a>
-			</p>
-			<p>
-				<strong>CTF rules:</strong>
-				<DocumentLink link={asset("/documents/2024-2025/events/liu_ctf/liu_ctf_rules.pdf")} />
-			</p>
-
-			<hr />
-			<div class="sponsor-logos">
-				<enhanced:img src={sickLogo} alt="sick logo" />
-				<enhanced:img src={sectraLogo} alt="sectra logo" />
-				<enhanced:img src={sylogLogo} alt="sylog logo" />
-			</div>
-		</div>
-	</Article>
-
-	<Article>
-		<div class="past-event">
-			<h1>Workshop</h1>
-			<enhanced:img src={studenthuset} alt="Studenthuset" />
-			<p>
-				We held a workshop in preparation for our CTF. During the workshop, we explained and
-				showcased example challenges that resembled the actual CTF challenges.
-			</p>
-			<p>
-				There were 80 available seats, and those who registered were provided free food, sponsored
-				by the companies below.
-			</p>
-			<p><strong>Date & Time:</strong> 27 Feb 2025, 17:15-20:00</p>
-			<p>
-				<strong>Location:</strong>
-				<a
-					href="https://use.mazemap.com/#v=1&config=liu&campusid=742&zlevel=6&center=15.577904,58.397057&zoom=18.9&sharepoitype=poi&sharepoi=1000927265"
-				>
-					SH62 and SH63 (Linköping University, Sweden)
-				</a>
-			</p>
-			<p>
-				<strong>Workshop tasks:</strong>
-				<DocumentLink
-					link={asset("/documents/2024-2025/events/workshop/workshop_challenges.zip")}
-				/>
-			</p>
-			<p>
-				<strong>Workshop presentation:</strong>
-				<DocumentLink
-					link={asset("/documents/2024-2025/events/workshop/workshop_presentation.pptx")}
-				/>
-			</p>
-
-			<hr />
-			<div class="sponsor-logos">
-				<enhanced:img src={sickLogo} alt="sick logo" />
-				<enhanced:img src={sectraLogo} alt="sectra logo" />
-			</div>
+		<hr />
+		<div class="sponsor-logos">
+			<enhanced:img src={sickLogo} alt="sick logo" />
+			<enhanced:img src={sectraLogo} alt="sectra logo" />
 		</div>
 	</Article>
 </ArticleGroup>
@@ -231,18 +221,6 @@
 
 		img {
 			height: 2rem;
-		}
-	}
-
-	.past-event {
-		opacity: 0.5;
-
-		img {
-			filter: saturate(50%);
-		}
-
-		h1 {
-			margin-top: 0;
 		}
 	}
 
