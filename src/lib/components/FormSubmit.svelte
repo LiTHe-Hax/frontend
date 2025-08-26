c7211b0fa76328dccfeb8e0a7d2ff1bd7847763c
Viktor Holta <viktor.holta@outlook.com>
2025-08-27 01:01:18 +0200

    Globalize the style of FormSubmit

--- src/lib/components/FormSubmit.svelte
+++ src/lib/components/FormSubmit.svelte
@@ -10,28 +10,3 @@
 		{@render children()}
 	{/if}
 </button>
-
-<style lang="scss">
-	@use "$lib/styles/color";
-	@use "$lib/styles/mixin";
-
-	button {
-		@include mixin.unified-transition(150ms, ease-out, background-color, color, transform);
-
-		padding: 0.5rem;
-		border: 0;
-		border-radius: 0.25rem;
-		background-color: color.$gray-1;
-		color: color.$green-2;
-		box-shadow: 0 2px 4px 0 color.$shadow;
-		font-size: 1em;
-		font-weight: bold;
-		transform: scaley(1);
-
-		&:hover {
-			background-color: color.$green-2;
-			color: color.$gray-1;
-			transform: scaley(1.1);
-		}
-	}
-</style>
