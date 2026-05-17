57cb2b79a8236f1d879d923947a8845d648b3897
Viktor Holta <30988624+parslie@users.noreply.github.com>
2026-05-17 20:03:59 +0200

    Use Status component to show status in EventPage

--- src/lib/components/pages/EventPage.svelte
+++ src/lib/components/pages/EventPage.svelte
@@ -2,19 +2,23 @@
 	import { page } from "$app/state";
 
 	import { getPreviousEvents, getUpcomingEvents, type Event } from "$lib/api/event";
+	import type { StatusType } from "$lib/types";
 
 	import ArticleGroup from "$lib/components/ArticleGroup.svelte";
 	import EventArticle from "$lib/components/EventArticle.svelte";
 	import Pagination from "$lib/components/Pagination.svelte";
+	import Status from "../Status.svelte";
 
 	type Props = { showPrevious?: boolean };
 	const { showPrevious }: Props = $props();
 
 	let currentPage = $state(1);
 	let pageCount = $state(1);
-	let events = $state<Event[] | undefined>(undefined);
-	let isLoading = $state(true);
-	let error = $state<string | undefined>(undefined);
+	let events = $state<Event[]>([]);
+
+	let showStatus = $state(true);
+	let statusMessage = $state("");
+	let statusType = $state<StatusType>("loading");
 
 	const getEvents = $derived(showPrevious ? getPreviousEvents : getUpcomingEvents);
 
@@ -28,78 +32,53 @@
 	});
 
 	$effect(() => {
-		isLoading = true;
-		error = undefined;
+		showStatus = true;
+		statusMessage = "";
+		statusType = "loading";
 
 		getEvents(currentPage)
 			.then((response) => {
 				if (response.isSuccessful) {
 					events = response.data!.results;
 					pageCount = response.data!.page_count;
+
 					if (events.length == 0) {
-						error = `There are no ${showPrevious ? "previous" : "upcoming"} events.`;
+						statusMessage = `There are no ${showPrevious ? "previous" : "upcoming"} events.`;
+						statusType = "error";
+					} else {
+						showStatus = false;
 					}
 				} else {
-					error = "There are no events on this page. Please go to a previous page.";
+					statusMessage = "There are no events on this page. Please go to a previous page.";
+					statusType = "error";
 				}
 			})
 			.catch(() => {
-				error = "Could not reach the server. Please try again later.";
-			})
-			.finally(() => {
-				isLoading = false;
+				statusMessage = "Could not connect to the server. Try again later.";
+				statusType = "error";
 			});
 	});
 </script>
 
 <ArticleGroup layout="thin">
-	{#if isLoading}
-		<div class="status-overlay">
-			<i class={["loading", "fa-solid", "fa-spinner", "fa-spin-pulse"]}></i>
-		</div>
-	{:else if error}
-		<div class="status-overlay">
-			<i class={["error", "fa-solid", "fa-heart-crack"]}></i>
-			<p>{error}</p>
-		</div>
+	{#if showStatus}
+		<Status message={statusMessage} type={statusType} />
 	{:else}
 		{#each events as event (event.title)}
 			<EventArticle {event} />
 		{/each}
 	{/if}
 
-	{#if events || !isLoading}
-		<div class={["pagination-container", "mobile"]}>
-			<Pagination {currentPage} {pageCount} buttonCount={3} />
-		</div>
-		<div class={["pagination-container", "desktop"]}>
-			<Pagination {currentPage} {pageCount} buttonCount={5} />
-		</div>
-	{/if}
+	<div class={["pagination-container", "mobile"]}>
+		<Pagination {currentPage} {pageCount} buttonCount={3} />
+	</div>
+	<div class={["pagination-container", "desktop"]}>
+		<Pagination {currentPage} {pageCount} buttonCount={5} />
+	</div>
 </ArticleGroup>
 
 <style lang="scss">
-	@use "$lib/styles/color";
 	@use "$lib/styles/mixin";
-	@use "$lib/styles/size";
-
-	.status-overlay {
-		text-align: center;
-
-		i {
-			display: block;
-			margin: 0 auto;
-			font-size: 2em;
-
-			&.error {
-				color: color.$error;
-			}
-		}
-
-		p {
-			margin: size.$spacing-xs 0 0;
-		}
-	}
 
 	.pagination-container {
 		&.mobile {
